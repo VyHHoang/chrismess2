3b956689dc4185df274ed160d091863889eda766
Vy H Hoang <vyhoang@vys-macbook-pro.coldspring.marian.edu>
2018-06-29 08:18:21 -0400

    Update a fave button

--- index.js
+++ index.js
@@ -16,28 +16,56 @@ class App {
       span.textContent = value
       return span
     }
-  
-    renderItem(flick) {
-      const item = document.createElement('li')
-      item.classList.add('flick')
-  
-      // get the list of properties
-      const properties = Object.keys(flick)
-  
-      // loop over each property
-      properties.forEach((propertyName) => {
-        // build a span
-        const span = this.renderProperty(propertyName, flick[propertyName])
-        item.appendChild(span)
-      })
-      const dButton = document.createElement('button')   
+    renderProperties(flick) {
+        const div = document.createElement('div')
+        div.classList.add('info')
+    
+        // get the list of properties
+        const properties = Object.keys(flick)
+    
+        // loop over each property
+        properties.forEach((propertyName) => {
+          // build a span
+          const span = this.renderProperty(propertyName, flick[propertyName])
+          div.appendChild(span)
+        })
+    
+        return div
+
+
+  renderActionButtons(flick, item) {
+    const actions = document.createElement('div')
+    actions.classList.add('actions')
+
+    const dButton = document.createElement('button')   
           dButton.textContent = "Delete"
           dButton.addEventListener('click',()=>this.removeFlick(item))
           item.appendChild(dButton)
 
-  
-      return item
-    }
+          const fButton = document.createElement('button')
+          fButton.textContent = "Love it!"
+          fButtonn. .addEventListener(
+            'click',
+            (_ev) => this.toggleFavorite(flick, item)
+            actions.appendChild(fButton)
+
+return actions
+          }
+          renderItem(flick) {
+            const item = document.createElement('li')
+            item.classList.add('flick')
+        
+           
+            const properties = this.renderProperties(flick)
+            item.appendChild(properties)
+        
+           
+            const actions = this.renderActionButtons(flick, item)
+            item.appendChild(actions)
+        
+            return item
+          }
+        
 removeFlick (item){
     this.list.removeChild(item)
 }
